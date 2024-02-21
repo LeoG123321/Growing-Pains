4c7a009c0b97caca3bc440f9fe95f373c41ba85b
LeoG123321 <3545gonzalez@gmail.com>
2024-02-20 19:41:20 -0600

    Add project files.

---
+++ Growing Pains/Growing Pains.cpp
@@ -0,0 +1,14 @@
+/*
+    Leonardo Gonzalez   2/20/2024
+    
+    Week 6              Growing Pains
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
